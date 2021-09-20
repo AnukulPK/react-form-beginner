a454fc709bd7f4f1f1f6be1a1c83b1d8364380c4
Anukul <anukul.p@growthsourceft.com>
2021-09-20 14:33:23 +0530

    success message integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleFirstNameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      firstName: event.target.value,
+    }));
+  };
+  const handleLastNameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      lastName: event.target.value,
+    }));
+  };
+  const handleEmailInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      email: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="first-name"
+          className="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={values.firstName}
+          onChange={handleFirstNameInputChange}
+        />
+        <input
+          id="last-name"
+          className="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={values.lastName}
+          onChange={handleLastNameInputChange}
+        />
+        <input
+          id="email"
+          className="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={values.email}
+          onChange={handleEmailInputChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {submitted && <div>Success, thank you for registering!</div>}
+    </>
   );
 }
 
